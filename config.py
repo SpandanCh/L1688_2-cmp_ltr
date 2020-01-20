cc80e53af741528d31730bd2df51c6e43521fc22
Spandan Choudhury <spandan@mpe.mpg.de>
2020-01-20 19:52:45 +0100

    added colour-blind friendly

--- config.py
+++ config.py
@@ -12,10 +12,8 @@ from aplpy import FITSFigure as aplfig
 
 import pickle 
 
-sort_reg = ['OphA' , 'OphC' , 'OphD' , 'OphE' , 'OphF' , 'd9' , 'd10' , 'd12', 'c4' , 'west' , 'top' , 'B_sou']
-reg_lab = {'OphA': 'Oph-A' ,'OphC': 'Oph-C' ,'OphD': 'Oph-D' ,'OphE': 'Oph-E' ,'OphF': 'Oph-F' ,'d9': 'Oph-H-MM1' ,
-       'd10': 'L1688-d10' ,'d12': 'L1688-d12' ,'c4': 'L1688-c4' ,'west': 'L1688-SR3' ,'top': 'L1688-SR2' ,'B_sou': 'L1688-SR1'}
-clr = {'OphA': 'k' ,'OphC': '#1f78b4' ,'OphD': '#b2df8a' ,'OphE': '#33a02c' ,'OphF': '#fb9a99' ,'d9': '#e31a1c' ,
-       'd10': '#fdbf6f' ,'d12': 'y' ,'c4': '#ff7f00' ,'west': '#cab2d6' ,'top': '#6a3d9a' ,'B_sou': '#b15928'}
-       
-
+sort_reg = ['OphA', 'OphC', 'OphD', 'OphE', 'OphF', 'd9', 'd10', 'd12', 'c4', 'west', 'top', 'B_sou']
+reg_lab = {'OphA': 'Oph-A', 'OphC': 'Oph-C', 'OphD': 'Oph-D', 'OphE': 'Oph-E', 'OphF': 'Oph-F', 'd9': 'Oph-H-MM1', 'd10': 'L1688-d10', 'd12': 'L1688-d12', 'c4': 'L1688-c4', 'west': 'L1688-SR3', 'top': 'L1688-SR2', 'B_sou': 'L1688-SR1'}
+clr = {'OphA': 'k', 'OphC': '#1f78b4', 'OphD': '#b2df8a', 'OphE': '#33a02c', 'OphF': '#fb9a99', 'd9': '#e31a1c', 'd10': '#fdbf6f', 'd12': 'y', 'c4': '#ff7f00', 'west': '#cab2d6', 'top': '#6a3d9a', 'B_sou': '#b15928'}
+clr_cbs = {'OphA': '#d73027', 'OphC': '#fc8d59', 'OphD': '#fee08b', 'OphE': '#1a9850', 'OphF': '#d9ef8b', 'd9': '#91cf60', 'd10': '#d73027', 'd12': '#fc8d59', 'c4': '#fee08b', 'west': '#d9ef8b', 'top': '#91cf60', 'B_sou': '#1a9850'}
+mrk_cbs = {'OphA': 'o', 'OphC': 'o', 'OphD': 'o', 'OphE': 'o', 'OphF': 'o', 'd9': 'o', 'd10': 's', 'd12': 's', 'c4': 's', 'west': 's', 'top': 's', 'B_sou': 's'}
